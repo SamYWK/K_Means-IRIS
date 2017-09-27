5f712f64e9542fd37208204a97e193d67e15c5ed
scan6 <pig840421@gmail.com>
2017-09-27 23:42:35 +0800

    First try

---
+++ k_means.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 27 23:25:03 2017
+
+@author: pig84
+"""
+
+import pandas as pd
+import numpy as np
+
+#load data
+x_data = pd.read_csv('irisDataSet.csv')
