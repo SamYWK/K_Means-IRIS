ec2f79f1b94e826b4e4610616c12a2f107a83dac
scan6 <pig840421@gmail.com>
2017-09-29 00:17:23 +0800

    Finish dealing with data

--- k_means.py
+++ k_means.py
@@ -7,6 +7,25 @@ Created on Wed Sep 27 23:25:03 2017
 
 import pandas as pd
 import numpy as np
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+import random
 
-#load data
-x_data = pd.read_csv('irisDataSet.csv')
+#data handling
+data = pd.read_csv('irisDataSet.csv')
+data = data.drop(['sepal length', 'sepal width'], axis = 1)    #Drop unwanted features
+x_data = data.drop('class', axis = 1)    #separate x from data
+y_data = data.drop(['petal length', 'petal width'], axis = 1)    #separate y from data
+#print(x_data)
+#print(y_data)
+#normalize features
+mms = preprocessing.MinMaxScaler()
+x = x_data.values   
+x = mms.fit_transform(x)
+x_data = pd.DataFrame(x, columns = ['petal length', 'petal width'])
+plt.scatter(x_data.values[:, 0], x_data.values[:, 1])
+plt.show()
+
+#k-means
+index_list = range(150)
+randPoints = random.sample(index_list, 2)    #To avoid selecting the same numbers
